ff553ed08e3bfe2f93af7a445cc6e3bd34ccc7ef
Raas-Farooq <raasfarooq@gmail.com>
2024-07-18 15:58:57 +0500

    add Navbar and other Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { useGlobalState } from "./Context";
+import { Navbar } from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/home";
+import { RecipeList } from "./Components/RecipeList";
+import { Detail } from "./Pages/detail";
 
 export default function App(){
     
-    console.log("useGlobal :", useGlobalState())
+    
     return (
+        <>
         <div>
-            <h2> Power Of Believe </h2>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="recipeList" element ={<RecipeList />} />
+                <Route path="/detail" element={<Detail />} />
+            </Routes>
         </div>
+        </>
+        
     )
 }
