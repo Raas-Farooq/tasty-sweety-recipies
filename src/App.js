696ff814a8f4c8598743028d91aeed8ef9ff5183
Raas-Farooq <raasfarooq@gmail.com>
2024-07-25 14:02:11 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { useGlobalState } from "./Context/index";
 import { Navbar } from "./Components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -12,18 +11,6 @@ import Favorites from './Pages/Favorites.js';
 export default function App(){
     
     const {recipesData} = useGlobalState();
-=======
-import { useGlobalState } from "./Context";
-import { Navbar } from "./Components/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/home";
-import { RecipeList } from "./Components/RecipeList";
-import { Detail } from "./Pages/detail";
-
-export default function App(){
-    
-    
->>>>>>> feature/navbar
     return (
         <>
         <div>
@@ -31,15 +18,10 @@ export default function App(){
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="recipeList" element ={<RecipeList />} />
-<<<<<<< HEAD
                 <Route path='/detail/:id' element={<Detail />} />
                 <Route path='/favorites' element ={<Favorites />} />
             </Routes>
            
-=======
-                <Route path="/detail" element={<Detail />} />
-            </Routes>
->>>>>>> feature/navbar
         </div>
         </>
         
